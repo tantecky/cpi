5cf0b5fde57f0c5551b8b9979cbe1705dc094a20
Tomas Antecky <tantecky@seznam.cz>
2021-12-18 10:05:48 +0100

    frontend: Add background pic

--- frontend/src/app/chart/chart.component.ts
+++ frontend/src/app/chart/chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CpiService, IDataPoint } from '../cpi.service';
-import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { ChartConfiguration, ChartData, ChartType, Plugin } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import 'chartjs-adapter-date-fns';
 import { cs } from 'date-fns/locale';
@@ -8,6 +8,9 @@ import { cs } from 'date-fns/locale';
 const LOCALE: string = 'cs-CZ';
 const LINE_COLOR: string = '#4f4675';
 const CURRENCY_SYMBOL: string = 'Kč';
+
+const image = new Image();
+image.src = 'assets/1k.jpg';
 
 @Component({
   selector: 'chart',
@@ -27,10 +30,21 @@ export class ChartComponent implements OnInit {
         pointBackgroundColor: LINE_COLOR,
         pointHoverBackgroundColor: '#ffffff',
         pointHoverBorderColor: LINE_COLOR,
+        fill: {
+          value: 1000,
+          below: '#ffffff',
+        },
       },
     ],
   };
   chartOptions: ChartConfiguration['options'] = {
+    animations: {
+      y: {
+        easing: 'linear',
+        duration: 1000,
+        delay: 500,
+      },
+    },
     locale: LOCALE,
     elements: {},
     parsing: false,
@@ -41,7 +55,7 @@ export class ChartComponent implements OnInit {
           tooltipFormat: 'd. M. yyyy',
         },
         grid: {
-          display: false,
+          display: true,
         },
         adapters: {
           date: {
@@ -52,7 +66,7 @@ export class ChartComponent implements OnInit {
       y: {
         beginAtZero: true,
         grid: {
-          display: false,
+          display: true,
         },
         ticks: {
           callback: function (value, index, values) {
@@ -86,6 +100,23 @@ export class ChartComponent implements OnInit {
       },
     },
   };
+
+  chartPlugins = [
+    {
+      id: 'custom_canvas_background_image',
+      beforeDraw: (chart: any) => {
+        if (image.complete) {
+          const ctx = chart.ctx;
+          const { top, left, width, height } = chart.chartArea;
+          const x = left + width / 2 - image.width / 2;
+          const y = top + height / 2 - image.height / 2;
+          ctx.drawImage(image, x, y);
+        } else {
+          image.onload = () => chart.draw();
+        }
+      },
+    },
+  ];
 
   constructor(private cpiService: CpiService) {}
 
